eb902956e8ed2cb95801f5a9bd1edfcbfbfc273a
Jean Carlos <berrocal13@gmail.com>
2018-04-16 08:20:40 -0600

    First Commit

---
+++ RegistroEstudianteBackEnd/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RegistroEstudianteBackEnd.Models;
+using Newtonsoft.Json;
+
+namespace RegistroEstudianteBackEnd.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string getAllStudents()
+        {
+
+            List<StudentModel> lstStudent = StudentModel.getAllStudents();
+
+            return JsonConvert.SerializeObject(lstStudent);
+
+        }
+    }
+}
